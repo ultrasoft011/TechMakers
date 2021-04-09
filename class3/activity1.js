c3a1249843f68ea23abe5e836219e35ad249b279
Andrés Felipe <ultrasoft.011@gmail.com>
2021-04-09 18:24:54 -0500

    resta

--- class3/activity1.js
+++ class3/activity1.js
@@ -3,3 +3,8 @@
 let number1 = Number(prompt('Digite el primer número'));
 let number2 = Number(prompt('Digite el segundo número'));
 console.log(number1 + number2);
+
+let number1 = Number(prompt('Digite el primer número'));
+let number2 = Number(prompt('Digite el segundo número'));
+console.log(number1 - number2);
+
